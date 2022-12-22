df8866b270d62d1bb8e1c3e3492187e4243d14ad
MacMakoto <102998595+Makoto335@users.noreply.github.com>
2022-12-22 17:38:37 +0900

    Associate memos table and Add validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,11 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   include DeviseTokenAuth::Concerns::User
+  has_many :memos
+  validates :name, presence: true
+  validates :name, presence: true, length: { maximum: 60 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 254 },
+                    format: { with: VALID_EMAIL_REGEX }
+  # validates :memo, presence: true, length: { maximum: 500 }
 end
