a5553e782ae2509c1c08535e27b36f7a8f8c8e2c
MacMakoto <102998595+Makoto335@users.noreply.github.com>
2022-12-22 10:52:37 +0900

    Create memo model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_12_20_224919) do
+ActiveRecord::Schema.define(version: 2022_12_22_014933) do
+
+  create_table "memos", force: :cascade do |t|
+    t.integer "user_id"
+    t.string "content"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_memos_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "provider", default: "email", null: false
