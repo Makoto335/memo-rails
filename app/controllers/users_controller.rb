43e8c5dce93b9e59c465f41bbc4be004dc265e4a
MacMakoto <102998595+Makoto335@users.noreply.github.com>
2022-12-29 17:50:23 +0900

    Add show method

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,18 @@
+class UsersController < ApplicationController
+  before_action :authenticate_user!, only: ["show"]
+
+  def show
+    user = User.find_by(email: request.headers["uid"])
+    memos = user.memos
+    memos_array =
+      memos.map do |memo|
+        {
+          title:memo.title,
+          content: memo.content,
+          created_at: memo.created_at,
+        }
+      end
+
+    render json: memos_array, status: 200
+  end
+end
