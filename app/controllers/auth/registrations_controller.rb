ee51b436a63120653cf1c186a2a5c5efffd7ea6c
MacMakoto <102998595+Makoto335@users.noreply.github.com>
2022-12-29 17:51:15 +0900

    Change sign_up_params

--- app/controllers/auth/registrations_controller.rb
+++ app/controllers/auth/registrations_controller.rb
@@ -3,6 +3,6 @@ class Auth::RegistrationsController < DeviseTokenAuth::RegistrationsController
   private
 
   def sign_up_params
-    params.permit(:name, :email, :password, :password_confirmation, :gender, :date_of_birth)
+    params.require(:registration).permit(:name, :email, :password, :password_confirmation, :gender, :date_of_birth)
   end
 end
