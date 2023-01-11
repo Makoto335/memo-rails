42c73a27b9e3bdd9c7bc0dd921eba62c945334ef
MacMakoto <102998595+Makoto335@users.noreply.github.com>
2023-01-11 15:30:03 +0900

    Add memo create action

--- app/controllers/memos_controller.rb
+++ app/controllers/memos_controller.rb
@@ -1,20 +1,19 @@
 class MemosController < ApplicationController
-  before_action :authenticate_user!, only: ["index","create"]
+  before_action :authenticate_user!, only: ["create"]
   
   def create
-    @memo = current_user.memos.build(memo_params)
-    # @memo.image.attach(params[:memo][:image])
-    if @memo.save
+    user = User.find_by(email: request.headers["uid"])
+    memo = user.memos.build(memo_params)
+    if memo.save
+      render json: {}, status: :ok
     else
-      @feed_items = current_user.feed.page(params[:page])
-      render 'static_pages/home'
+      render json: {}, status: :bad_request
     end
   end
 
   private
 
   def memo_params
-    # params.require(:memo).permit(:title, :content)
-    params.permit(:title, :content)
+    params.require(:memo).permit(:title, :content)
   end
 end
