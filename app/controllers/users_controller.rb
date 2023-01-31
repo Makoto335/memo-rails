6b5e2c83d2bb7e22cd5982ddc2a6c13569f7f130
MacMakoto <102998595+Makoto335@users.noreply.github.com>
2023-01-31 17:32:11 +0900

    Add  avatar_url in the hash as json

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,19 +1,24 @@
 class UsersController < ApplicationController
-  before_action :authenticate_user!, only: ["show"]
+  before_action :authenticate_user!, only: ['show']
 
   def show
-    user = User.find_by(email: request.headers["uid"])
+    user = User.find_by(email: request.headers['uid'])
     memos = user.memos
     memos_array =
       memos.map do |memo|
         {
-          id:memo.id,
-          title:memo.title,
+          id: memo.id,
+          title: memo.title,
           content: memo.content,
           created_at: memo.created_at,
         }
       end
-
-    render json: memos_array, status: 200
+    avatar_url = user.avatar_url
+    render json: {
+             'user' => {
+               'memos_array' => memos_array,
+               'avatar_url' => avatar_url,
+             },
+           }, status: 200
   end
 end
