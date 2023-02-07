c6f1f81ba5c1059e8056a95bd95b2defb8d69e05
MacMakoto <102998595+Makoto335@users.noreply.github.com>
2023-02-07 17:55:27 +0900

    Add default avatar feature and change validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 class User < ActiveRecord::Base
   include Rails.application.routes.url_helpers
+  before_create :default_avatar
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
@@ -14,25 +15,59 @@ class User < ActiveRecord::Base
   has_one_attached :avatar
   validates :name, presence: true
   validates :name, presence: true, length: { maximum: 60 }
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{8,100}+\z/i
   validates :email,
             presence: true,
             length: {
               maximum: 254,
             },
             format: {
-              with: VALID_EMAIL_REGEX,
+              with: URI::MailTo::EMAIL_REGEXP,
             }
+  validates :password,
+            length: {
+              maximum: 254,
+            },
+            format: {
+              with: VALID_PASSWORD_REGEX,
+            },
+            allow_blank: true
   validates :avatar,
             content_type: {
-              in: [:png, :jpg, :jpeg],
+              in: %i[png jpg jpeg gif],
               message: 'png, jpg, jpegいずれかの形式にして下さい',
             },
             size: {
-              less_than: 5.megabytes,
+              less_than: 2.megabytes,
               message: 'サイズを5M以下にしてください',
             }
+
   def avatar_url
-    avatar.attached? ? url_for(avatar) : nil
+    if avatar.attached?
+      resized_avatar = avatar.variant(resize_to_limit: [400, 400])
+      url_for(resized_avatar)
+    else
+      return nil
+    end
+  end
+
+  private
+
+  def default_avatar
+    if !self.avatar.attached? && self.new_record?
+      self.avatar.attach(
+        io:
+          File.open(
+            Rails.root.join(
+              'app',
+              'assets',
+              'images',
+              'blank-profile-picture_640.png',
+            ),
+          ),
+        filename: 'blank-profile-picture_640.png',
+        content_type: 'image/png',
+      )
+    end
   end
 end
